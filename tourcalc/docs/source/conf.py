979360e926eb7495872faeeed4114d56afdd0086
Claudia Behnke <claudia.behnke@surfsara.nl>
2021-11-19 19:52:46 +0100

    fixing docs

--- tourcalc/docs/source/conf.py
+++ tourcalc/docs/source/conf.py
@@ -22,7 +22,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import tourcalc
+#import tourcalc
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
