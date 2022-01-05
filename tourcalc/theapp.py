50d7b4e5fd6a5e8b179e22f505cabd763393c28f
Claudia Behnke <claudia.behnke@surfsara.nl>
2022-01-05 17:45:20 +0100

    try to read in files

--- tourcalc/theapp.py
+++ tourcalc/theapp.py
@@ -185,7 +185,7 @@ cat_dict_day = {}  # day per category
 tour_name = st.text_input("Name of the tournament", key='unique')
 fname = tour_name + ".txt"
 check_file = Path('example_tours') / fname
-filenames = os.listdir('example_tours')
+filenames = os.listdir('.')
 st.write(filenames)
 permutations_object = itertools.permutations(DIS_INP)
 permutations_list = list(permutations_object)
